0c26ccc2b1f342b0164281a8ccdef982d0630c98
karmuno <cody.jane.stahl@gmail.com>
2025-04-18 19:28:11 -0400

    Added original map and started building tileset. Still not appearing in GameScene though.

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,10 +4,18 @@ class GameScene extends Phaser.Scene {
     }
   
     preload() {
-      console.log("GameScene loaded!"); 
+      console.log("GameScene loaded!");
+      // Load the map tileset image and the tilemap JSON
+      this.load.image('original-map', 'assets/images/board/original-map-upscaled.png');
+      this.load.tilemapTiledJSON('map', 'assets/tilemaps/sheboygan_map.json');
     }
   
     create() {
-    //   this.scene.start('MainMenuScene'); 
+      // Create the tilemap and display the background layer
+      const map = this.make.tilemap({ key: 'map' });
+      const tileset = map.addTilesetImage('original-map', 'original-map');
+      const background = map.createLayer('Background', tileset, 0, 0);
+      
+      // TODO: Add additional layers, objects, and game logic here
     }
   }
